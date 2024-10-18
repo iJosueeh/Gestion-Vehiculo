c9acb750eba05539081bffa0e8880c17e0d7823e
iJosueeh <royer.tanta27@gmail.com>
2024-10-18 11:31:22 -0500

    Actualizando la clase Main el menú

--- src/Main.java
+++ src/Main.java
@@ -6,56 +6,93 @@ import modelos.Vehiculo;
 import servicios.GestionarVehiculos;
 
 public class Main {
+
     public static void main(String[] args) {
         GestionarVehiculos gestionarVehiculos = new GestionarVehiculos();
+        Vehiculo vehiculo;
         Scanner scanner = new Scanner(System.in);
-        
+
         boolean continuar = true;
-        
+
         do {
-            System.out.println("¿Cuántos vehiculos desea ingresar?: ");
+            System.out.print("¿Cuántos vehiculos desea ingresar?: ");
             int cantidadVehiculos = scanner.nextInt();
-            
+            scanner.nextLine();
+
             for (int i = 0; i < cantidadVehiculos; i++) {
-                System.out.println("Ingresa los datos del vehiculo " + (i+1) + ": \n");
+                System.out.println("Ingresa los datos del vehiculo " + (i + 1) + ": \n");
+
                 System.out.print("Marca: ");
                 String marca = scanner.nextLine();
+
                 System.out.print("Modelo: ");
                 String modelo = scanner.nextLine();
-                
-                scanner.nextLine();
-                
+
                 System.out.println("¿Tipo de Vehiculo?");
                 System.out.println("1. Coche");
                 System.out.println("2. Moto");
                 System.out.print("Elija la opción disponible: ");
-                String opcionMenu = scanner.nextLine();
-                
-                if (opcionMenu.equalsIgnoreCase("coche") && opcionMenu.equals("1")) {
+                String opcionMenu = scanner.nextLine().toLowerCase();
+
+                if (opcionMenu.equals("1")) {
                     System.out.print("Número de puertas: ");
                     int numeroPuertas = scanner.nextInt();
-                    Vehiculo vehiculo = new Coche(marca, modelo, numeroPuertas);
+                    scanner.nextLine();
+                    vehiculo = new Coche(marca, modelo, numeroPuertas);
                     gestionarVehiculos.agregarVehiculo(vehiculo);
-                } else if (opcionMenu.equalsIgnoreCase("moto") && opcionMenu.equalsIgnoreCase("2")) {
-                    System.out.println("Cilindraje (cc): ");
+                } else if (opcionMenu.equals("2")) {
+                    System.out.print("Cilindraje (cc): ");
                     int cilindraje = scanner.nextInt();
-                    Vehiculo vehiculo = new Motos(marca, modelo, cilindraje);
+                    scanner.nextLine();
+                    vehiculo = new Motos(marca, modelo, cilindraje);
                     gestionarVehiculos.agregarVehiculo(vehiculo);
+                } else {
+                    System.out.println("Opción no válida.");
                 }
-                
             }
-            
+
             System.out.println("Lista de Vehiculos almacenados: ");
             gestionarVehiculos.mostrarVehiculos();
-            
+
             System.out.println("\n¿Desea eliminar un Vehiculo?: ");
             System.out.println("1. Si");
             System.out.println("2. No");
+            System.out.print("Seleccione la opción disponible: ");
             String opcionEliminar = scanner.nextLine();
-            
-            if (opcionEliminar.equalsIgnoreCase("si") && opcionEliminar.equalsIgnoreCase("1")) {
+
+            if (opcionEliminar.equalsIgnoreCase("si") || opcionEliminar.equals("1")) {
+                System.out.print("Escriba el ID del vehículo: ");
+                int idVehiculo = scanner.nextInt();
                 
+                scanner.nextLine();
+
+                Vehiculo vehiculoEliminar = null;
+
+                for (Vehiculo vehi : gestionarVehiculos.getVehiculos()) {
+                    if (vehi.getIdVehiculo() == idVehiculo) {
+                        vehiculoEliminar = vehi;
+                        break;
+                    }
+                }
+
+                if (vehiculoEliminar != null) {
+                    gestionarVehiculos.eliminarVehiculo(vehiculoEliminar);
+                } else {
+                    System.out.println("No se ha encontrado un vehículo con ese ID.");
+                }
             }
-        } while (!continuar);
+
+            System.out.println("¿Deseas continuar en el programa?: ");
+            System.out.println("1. Si");
+            System.out.println("2. No");
+            System.out.print("Seleccione la opción correcta.");
+            String opcionContinuar = scanner.nextLine();
+
+            if (opcionContinuar.equals("2")) {
+                System.out.println("Saliendo del programa... ¡vuelve pronto!");
+                continuar = false;
+            }
+
+        } while (continuar);
     }
 }
